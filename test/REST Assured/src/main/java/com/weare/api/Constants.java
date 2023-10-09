7316aad022a4cc34e0aa9b21bdccbca3dd92cd8b
FGargov <fgargov@gmail.com>
2023-10-09 10:14:21 +0300

    Add constants for registration

--- test/REST Assured/src/main/java/com/weare/api/Constants.java
+++ test/REST Assured/src/main/java/com/weare/api/Constants.java
@@ -1,4 +1,19 @@
 package com.weare.api;
 
 public class Constants {
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "P@ssw0rd";
+    public static final String AUTHORITIES = "RULE_USER";
+    public static String USER_ID = "";
+    public static String CATEGORY_ID = "102";
+
+    public static final String EMAIL = "";
+    public static final String FIRST_NAME = "";
+    public static final String LAST_NAME = "";
+    public static final String birthYear = "";
+
+    public static final String FULL_NAME = FIRST_NAME + " " + LAST_NAME;
+    public static final String EXPERTISE = "All";
+
+
 }
