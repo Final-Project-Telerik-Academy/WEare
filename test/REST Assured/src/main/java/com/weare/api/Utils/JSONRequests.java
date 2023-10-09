358dfeecab47ef9af9b66c8a2777dad6b771e3f6
FGargov <fgargov@gmail.com>
2023-10-09 11:34:25 +0300

    Add JSON body for registration

--- test/REST Assured/src/main/java/com/weare/api/Utils/JSONRequests.java
+++ test/REST Assured/src/main/java/com/weare/api/Utils/JSONRequests.java
@@ -1,5 +1,17 @@
 package com.weare.api.Utils;
 
 public class JSONRequests {
-
+    public static final String REGISTRATION = "{\n" +
+            "  \"authorities\": [\n" +
+            "    \"%s\"\n" +
+            "  ],\n" +
+            "  \"category\": {\n" +
+            "    \"id\": %s,\n" +
+            "    \"name\": \"%s\"\n" +
+            "  },\n" +
+            "  \"confirmPassword\": \"%s\",\n" +
+            "  \"email\": \"%s\",\n" +
+            "  \"password\": \"%s\",\n" +
+            "  \"username\": \"%s\"\n" +
+            "}";
 }
