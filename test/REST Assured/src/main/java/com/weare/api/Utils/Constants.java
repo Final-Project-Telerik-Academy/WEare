b26006ab8548f01f0ea10609fcabd55a03ca881a
Kristina-Duhneva <kristina.ivailova.d@gmail.com>
2023-10-09 23:56:15 +0300

    add authentication test

--- test/REST Assured/src/main/java/com/weare/api/Utils/Constants.java
+++ test/REST Assured/src/main/java/com/weare/api/Utils/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static String USERNAME = "";
     public static final String PASSWORD = "P@ssw0rd";
     public static final String AUTHORITIES = "RULE_USER";
+    public static  String COOKIE="";
     public static String USER_ID = "";
     public static String CATEGORY_ID = "102";
 
