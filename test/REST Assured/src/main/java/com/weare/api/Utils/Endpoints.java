d2730ebe3950876ce85f0174e340b684fd489937
FGargov <fgargov@gmail.com>
2023-10-09 13:58:24 +0300

    Fix endpoint mistake for registration.

--- test/REST Assured/src/main/java/com/weare/api/Utils/Endpoints.java
+++ test/REST Assured/src/main/java/com/weare/api/Utils/Endpoints.java
@@ -5,7 +5,6 @@ import static java.lang.String.format;
 public class Endpoints {
     public static final String BASE_URL="localhost:8081";
     public static final String AUTH_ENDPOINT ="/authenticate";
-    public static final String CREATE_USER ="/api/users/";
+    public static final String REGISTER_ENDPOINT ="/api/users/";
 
-    public static final String REGISTRATION_ENDPOINT = format("%s", CREATE_USER);
 }
---
+++ test/REST Assured/src/test/java/weare/api/tests/RegistrationTest.java
@@ -0,0 +1,16 @@
+package weare.api.tests;
+
+import base.BaseTestSetup;
+import org.testng.annotations.Test;
+
+import static com.weare.api.Utils.Endpoints.BASE_URL;
+import static com.weare.api.Utils.Endpoints.REGISTER_ENDPOINT;
+import static io.restassured.RestAssured.baseURI;
+import static java.lang.String.format;
+
+public class RegistrationTest extends BaseTestSetup {
+    @Test
+    public void successfulRegistration_when_createNewUser() {
+        baseURI = format("%s%s", BASE_URL, REGISTER_ENDPOINT);
+    }
+}
