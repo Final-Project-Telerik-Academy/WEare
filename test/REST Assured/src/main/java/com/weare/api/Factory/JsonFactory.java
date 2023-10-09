aa109b306d610416064359a8c2179ccb974c0865
FGargov <fgargov@gmail.com>
2023-10-09 22:22:13 +0300

    Add GsonFactory

--- test/REST Assured/src/main/java/com/weare/api/Factory/JsonFactory.java
+++ test/REST Assured/src/main/java/com/weare/api/Factory/JsonFactory.java
@@ -1,4 +1,14 @@
 package com.weare.api.Factory;
+import com.google.gson.Gson;
 
 public class JsonFactory {
+    private static final Gson gson = new Gson();
+
+    public static <T> String toJson(T object) {
+        return gson.toJson(object);
+    }
+
+    public static <T> T fromJson(String json, Class<T> classOfT) {
+        return gson.fromJson(json, classOfT);
+    }
 }
