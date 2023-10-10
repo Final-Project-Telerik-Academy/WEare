8ef11ef1725c53eabf6641a981b243ceeea24517
FGargov <fgargov@gmail.com>
2023-10-10 13:09:30 +0300

    Fix BASE_URL

--- test/REST Assured/src/main/java/com/weare/api/Utils/Endpoints.java
+++ test/REST Assured/src/main/java/com/weare/api/Utils/Endpoints.java
@@ -3,7 +3,7 @@ package com.weare.api.Utils;
 import static java.lang.String.format;
 
 public class Endpoints {
-    public static final String BASE_URL="localhost:8081";
+    public static final String BASE_URL="http://localhost:8081";
     public static final String AUTH_ENDPOINT ="/authenticate";
     public static final String REGISTER_ENDPOINT ="/api/users/";
 
