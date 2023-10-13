8c2d405bcad969df010215c17c0a91ce4e33d52c
FGargov <fgargov@gmail.com>
2023-10-13 16:05:41 +0300

    Add Skill.java untracked file

---
+++ test/REST Assured/src/main/java/com/weare/api/Models/Skill.java
@@ -0,0 +1,37 @@
+package com.weare.api.Models;
+
+import com.github.javafaker.Faker;
+import com.weare.api.Utils.Constants;
+
+
+public class Skill {
+    private Integer id;
+    private String name;
+    private final Faker faker = new Faker();
+
+    public Skill() {
+        this.id = getId();
+        this.name = getRandomSkillName();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String getRandomSkillName() {
+
+        return faker.name().firstName();
+    }
+}
