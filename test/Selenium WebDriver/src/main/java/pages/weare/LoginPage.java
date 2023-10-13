90e35c6c71e681830ca942693dffea801455dd47
rosicatm <rr_markova@abv.bg>
2023-10-13 18:22:20 +0300

    Add proper asserts in Login and Registration pages

--- test/Selenium WebDriver/src/main/java/pages/weare/LoginPage.java
+++ test/Selenium WebDriver/src/main/java/pages/weare/LoginPage.java
@@ -8,41 +8,27 @@ public class LoginPage extends BaseWEArePage {
     }
 
     public void loginUser(String username, String password) {
-
-        //SIGN IN button
         actions.waitForElementVisible("weare.signInButton");
         actions.clickElement("weare.signInButton");
-
-        //username
         actions.waitForElementVisible("weare.loginUsername");
         actions.typeValueInField(username, "weare.loginUsername");
-
-        //password
         actions.waitForElementVisible("weare.loginPassword");
         actions.typeValueInField(password, "weare.loginPassword");
-
-        //submit form
         actions.clickElement("weare.LoginSubmitButton");
     }
 
     public void logoutUser() {
-
-        //Home section
         actions.waitForElementVisible("weare.homeButton");
         actions.clickElement("weare.homeButton");
-
-        //Logout button
         actions.waitForElementVisible("weare.logoutButton");
         actions.clickElement("weare.logoutButton");
     }
 
     public void assertUserIsLoggedIn() {
-        //Find your professional is present
-        actions.waitForElementPresent("weare.findYourProfessional");
+        actions.assertElementPresent("weare.findYourProfessional");
     }
 
     public void assertUserIsLoggedOut() {
-        //Find your professional is present
-        actions.waitForElementPresent("weare.assertLoggedOutText");
+        actions.assertElementPresent("weare.assertLoggedOutText");
     }
 }
