bec1b20a64d4c169a74d5b1fe19bd60bac3f8fac
rosicatm <rr_markova@abv.bg>
2023-10-13 19:15:39 +0300

    Add generateRandomString method in the BasePage

--- test/Selenium WebDriver/src/main/java/pages/weare/BaseWEArePage.java
+++ test/Selenium WebDriver/src/main/java/pages/weare/BaseWEArePage.java
@@ -3,8 +3,26 @@ package pages.weare;
 import com.telerikacademy.testframework.pages.BasePage;
 import org.openqa.selenium.WebDriver;
 
+import java.security.SecureRandom;
+
 public abstract class BaseWEArePage extends BasePage {
+
+    protected static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
     public BaseWEArePage(WebDriver driver, String urlKey) {
         super(driver, "weare.homepage");
+    }
+
+    protected static String generateRandomString(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder stringBuilder = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            stringBuilder.append(randomChar);
+        }
+
+        return stringBuilder.toString();
     }
 }
