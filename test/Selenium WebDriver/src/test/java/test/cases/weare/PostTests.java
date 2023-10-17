b51fc14f496a999bf1a29f729180d4d2b614efe0
rosicatm <rr_markova@abv.bg>
2023-10-17 21:21:42 +0300

    Remove Order annotations in PostTests

--- test/Selenium WebDriver/src/test/java/test/cases/weare/PostTests.java
+++ test/Selenium WebDriver/src/test/java/test/cases/weare/PostTests.java
@@ -18,70 +18,60 @@ public class PostTests extends BaseTest {
     }
 
     @Test
-    @Order(1)
     public void createPublicPost() {
         postPage.createPublicPost();
         postPage.assertPublicPostCreated();
     }
 
     @Test
-    @Order(2)
     public void createPrivatePost() {
         postPage.createPrivatePost();
         postPage.assertPrivatePostCreated();
     }
 
     @Test
-    @Order(3)
     public void createPostWithOneCharacter() {
         postPage.createPostWithOneCharacter();
         postPage.assertPostWithOneCharacterCreated();
     }
 
     @Test
-    @Order(4)
     public void createPostWith999Characters() {
         postPage.createPostWith999Character();
         postPage.assertPostWith999CharactersCreated();
     }
 
     @Test
-    @Order(5)
     public void createPostWith1000Characters() {
         postPage.createPostWith1000Character();
         postPage.assertPostWith1000CharactersCreated();
     }
 
     @Test
-    @Order(6)
     public void createPostWithPngPhoto() {
         postPage.createPostWithPngFormatPhoto();
         postPage.assertPostCreatedWithPhoto();
     }
 
     @Test
-    @Order(7)
     public void createPostWithJpgPhoto() {
         postPage.createPostWithJpgFormatPhoto();
         postPage.assertPostCreatedWithPhoto();
     }
 
     @Test
-    @Order(8)
     public void createPostWithPanoramicPhoto() {
         postPage.createPostWithPanoramicPhoto();
         postPage.assertPostCreatedWithPhoto();
     }
 
     @Test
-    @Order(9)
     public void createPostWithTextAndPhoto() {
         postPage.createPostWithTextAndPhoto();
         postPage.assertPostCreatedWithTextAndPhoto();
     }
 
     @Test
-    @Order(10)
     public void likePost() {
         postPage.createPublicPost();
         postPage.likePost();
@@ -89,7 +79,6 @@ public class PostTests extends BaseTest {
     }
 
     @Test
-    @Order(11)
     public void dislikePost() {
         postPage.createPublicPost();
         postPage.dislikePost();
@@ -97,21 +86,18 @@ public class PostTests extends BaseTest {
     }
 
     @Test
-    @Order(12)
     public void likeSpecificPost() {
         postPage.likeSpecificPost();
         postPage.assertSpecificPostIsLiked();
     }
 
     @Test
-    @Order(13)
     public void dislikeSpecificPost() {
         postPage.dislikeSpecificPost();
         postPage.assertSpecificPostIsDisliked();
     }
 
     @Test
-    @Order(14)
     public void editPostContent() {
         postPage.createPublicPost();
         postPage.editPostContent();
@@ -119,7 +105,6 @@ public class PostTests extends BaseTest {
     }
 
     @Test
-    @Order(15)
     public void deletePost() {
         postPage.createPublicPost();
         postPage.deletePost();
