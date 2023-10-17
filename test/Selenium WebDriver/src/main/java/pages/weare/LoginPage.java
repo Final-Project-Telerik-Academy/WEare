a2f0b3213bb8675d3283652d3764d517397dc016
rosicatm <rr_markova@abv.bg>
2023-10-17 13:03:46 +0300

    Add unhappy path tests for Login functionality

--- test/Selenium WebDriver/src/main/java/pages/weare/LoginPage.java
+++ test/Selenium WebDriver/src/main/java/pages/weare/LoginPage.java
@@ -2,6 +2,7 @@ package pages.weare;
 
 import org.openqa.selenium.WebDriver;
 
+
 public class LoginPage extends BasePage {
     public LoginPage(WebDriver driver) {
         super(driver, "weare.homepage");
@@ -14,6 +15,36 @@ public class LoginPage extends BasePage {
         actions.typeValueInField(username, "weare.loginUsername");
         actions.waitForElementVisible("weare.loginPassword");
         actions.typeValueInField(password, "weare.loginPassword");
+        actions.clickElement("weare.LoginSubmitButton");
+    }
+
+    public void loginUserWithInvalidUsername(String invalidUsername, String validPassword) {
+        actions.waitForElementVisible("weare.signInButton");
+        actions.clickElement("weare.signInButton");
+        actions.waitForElementVisible("weare.loginUsername");
+        actions.typeValueInField(invalidUsername, "weare.loginUsername");
+        actions.waitForElementVisible("weare.loginPassword");
+        actions.typeValueInField(validPassword, "weare.loginPassword");
+        actions.clickElement("weare.LoginSubmitButton");
+    }
+
+    public void loginUserWithInvalidPassword(String validUsername, String invalidPassword) {
+        actions.waitForElementVisible("weare.signInButton");
+        actions.clickElement("weare.signInButton");
+        actions.waitForElementVisible("weare.loginUsername");
+        actions.typeValueInField(validUsername, "weare.loginUsername");
+        actions.waitForElementVisible("weare.loginPassword");
+        actions.typeValueInField(invalidPassword, "weare.loginPassword");
+        actions.clickElement("weare.LoginSubmitButton");
+    }
+
+    public void loginUserWithInvalidCredentials(String invalidUsername, String invalidPassword) {
+        actions.waitForElementVisible("weare.signInButton");
+        actions.clickElement("weare.signInButton");
+        actions.waitForElementVisible("weare.loginUsername");
+        actions.typeValueInField(invalidUsername, "weare.loginUsername");
+        actions.waitForElementVisible("weare.loginPassword");
+        actions.typeValueInField(invalidPassword, "weare.loginPassword");
         actions.clickElement("weare.LoginSubmitButton");
     }
 
@@ -31,4 +62,9 @@ public class LoginPage extends BasePage {
     public void assertUserIsLoggedOut() {
         actions.assertElementPresent("weare.assertLoggedOutText");
     }
+    public void assertUserNotLoggedInWithInvalidCredentials() {
+        actions.assertElementPresent("weare.wrongUsernameOrPassword");
+    }
 }
+
+
--- test/Selenium WebDriver/src/test/java/test/cases/weare/LoginTest.java
+++ test/Selenium WebDriver/src/test/java/test/cases/weare/LoginTest.java
@@ -2,8 +2,42 @@ package test.cases.weare;
 
 import org.junit.jupiter.api.Test;
 
-public class LoginTest {
+import static com.telerikacademy.testframework.RandomGenerator.*;
+
+public class LoginTest extends BaseTest {
+
+    String invalidUsername = generateRandomUsername(1);
+    String invalidPassword = generateRandomPassword(5);
     @Test
-    public void login() {
+    public void loginWithInvalidUsername() {
+        username = generateRandomUsername(2);
+        email = generateRandomEmail();
+        password = generateRandomPassword(12);
+        registrationPage.userRegistration(username, email, password);
+        registrationPage.assertUserRegistered();
+        loginPage.loginUserWithInvalidUsername(invalidUsername, password);
+        loginPage.assertUserNotLoggedInWithInvalidCredentials();
+    }
+
+    @Test
+    public void loginWithInvalidPassword() {
+        username = generateRandomUsername(2);
+        email = generateRandomEmail();
+        password = generateRandomPassword(12);
+        registrationPage.userRegistration(username, email, password);
+        registrationPage.assertUserRegistered();
+        loginPage.loginUserWithInvalidPassword(username, invalidPassword);
+        loginPage.assertUserNotLoggedInWithInvalidCredentials();
+    }
+
+    @Test
+    public void loginWithInvalidCredentials() {
+        username = generateRandomUsername(2);
+        email = generateRandomEmail();
+        password = generateRandomPassword(12);
+        registrationPage.userRegistration(username, email, password);
+        registrationPage.assertUserRegistered();
+        loginPage.loginUserWithInvalidCredentials(invalidUsername, invalidPassword);
+        loginPage.assertUserNotLoggedInWithInvalidCredentials();
     }
 }
