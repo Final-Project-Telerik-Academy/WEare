4e6b0483567bdaefaa2937157f18a775def5a225
rosicatm <rr_markova@abv.bg>
2023-10-18 17:56:40 +0300

    Edit BasePage

--- test/Selenium WebDriver/src/main/java/pages/weare/BasePage.java
+++ test/Selenium WebDriver/src/main/java/pages/weare/BasePage.java
@@ -2,7 +2,18 @@ package pages.weare;
 
 import org.openqa.selenium.WebDriver;
 
+import java.time.LocalDate;
+
+import static com.telerikacademy.testframework.RandomGenerator.*;
+
 public abstract class BasePage extends com.telerikacademy.testframework.pages.BasePage {
+
+    LocalDate randomBirthday = LocalDate.parse("1990-12-01");
+//    LocalDate randomBirthday = generateRandomBirthday(1930, 2000);
+    protected String firstName = generateRandomFirstName();
+    protected String lastName = generateRandomLastName();
+
+    protected String fullName = firstName + " " + lastName;
 
     public BasePage(WebDriver driver, String urlKey) {
         super(driver, urlKey);
